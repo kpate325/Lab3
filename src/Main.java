05763b381e567c9fbb4197b13c7ff3f9a20b6801
Keyur Patel <kpate325.edu>
2018-09-19 16:54:23 -0500

    Push 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
-
     public static void main(String[] args) {
-	System.out.println("Here");
+        System.out.println(WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        String text = WebScraper.urlToString("https://www.bls.gov/tus/charts/chart9.txt");
+        String[] parts = text.split(" ");
+        int counter = 0;
+        for (int i =0; i < text.length(); i++) {
+            if (parts[i] != " ") {
+                counter++;
+            }
+        }
+        System.out.print(counter);
     }
 }
